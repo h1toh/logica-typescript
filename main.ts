0ef9723c15c9e93d19efc828483793d77c10806f
Lucas Gomes <lukashitoh@outlook.com>
2025-01-25 18:31:50 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -31,18 +31,45 @@ function newUser(username:string, email:string, pass:string, cellphone?:string):
   console.log(username, email, pass, cellphone)
 }
 newUser("lucas", "lucas@email.com","senha")
-// functions
 
-if(pessoa.nome=="lucas"){
-  falar(pessoa.nome)
-} else {
-  falar("estranho")
+const soma = (a: number, b: number): number => {
+    return a + b
+}
+soma(1, 1)
+
+const teste = (txt: string): void => {
+    console.log('Hello, world!')
+}
+teste("opa")
+
+function fsoma(...n: number[]) {
+    let i: number = 0
+    n.forEach((e) => {
+        i += e
+    })
+    return i
 }
 
-// condicoes
+console.log(fsoma(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+// functions
 
-arr.forEach((e)=>{
-  console.log(e)
-})
+for (let l of lista) {
+    console.log(l)
+}
 
-// loop
+interface info {
+    curso: string;
+    des?: string;
+    inicarCurso?(curso: string):void;
+}
+
+interface newInfo extends info {
+    aulas: number;
+    maxAluno?: number;
+}
+
+let info1: newInfo
+let info2: newInfo
+
+info1 = { curso: "Typescript", des: "legal", aulas: 200, maxAluno: 50 }
+info2 = { curso: "C++", des: "hacker", aulas: 100, maxAluno: 30 }
